af0d030c7b5704100ef2e63dfbfe3cadd95ab548
Se7en19 <hector.camarilloa@gmail.com>
2025-09-02 08:22:38 -0600

    Usar cross-entropy en backprop

--- network.py
+++ network.py
@@ -102,9 +102,8 @@ class Network(object):
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        # backward pass (cross-entropy + sigmoid en la salida)
+        delta = activations[-1] - y
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
